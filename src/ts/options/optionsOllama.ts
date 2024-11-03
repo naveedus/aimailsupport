8c46469e2efcf087860981fe8305955f33a0b135
YS Dev <dev@yellowsakura.com>
2024-11-03 22:01:15 +0100

    Continuation of Work Version 0.88
    	•	Library updates;
    	•	Progress with Ollama with the addition of error handling during configuration.

--- src/ts/options/optionsOllama.ts
+++ src/ts/options/optionsOllama.ts
@@ -15,29 +15,51 @@ document.querySelector('#ollamaListModel').addEventListener('click', async _ => 
     const selectedValue = selectOllamaModel.value || (await getConfig('ollama')).model
     selectOllamaModel.innerHTML = ''
 
+    // Removal of any previously displayed API error message
+    document.querySelector('#ollama .description.ollama-error-api').classList.remove('show')
+
+    // Removal of any previously displayed message to indicate that
+    // no model was found in Ollama.
+    document.querySelector('#ollama .description.ollama-warning-no-model').classList.remove('show')
+
     try {
         const ollamaLocalModels = await OllamaProvider.getLocalModels()
 
-        // Sort the array by the 'name' field
-        ollamaLocalModels.sort((a, b) => a.name.localeCompare(b.name))
+        if (ollamaLocalModels.length != 0) {
+            // Sort the array by the 'name' field
+            ollamaLocalModels.sort((a, b) => a.name.localeCompare(b.name))
 
-        // Add the newly retrieved models
-        ollamaLocalModels.forEach(model => {
-            const option = document.createElement('option')
-            option.textContent = model.name
-            option.value = model.model
+            // Add the newly retrieved models
+            ollamaLocalModels.forEach(model => {
+                const option = document.createElement('option')
+                option.textContent = model.name
+                option.value = model.model
 
-            // Restore the previously selected model (if any)
-            if (selectedValue && model.name == selectedValue) {
-                option.selected = true
-            }
+                // Restore the previously selected model (if any)
+                if (selectedValue && model.name == selectedValue) {
+                    option.selected = true
+                }
 
-            selectOllamaModel.appendChild(option)
-        })
-    } catch (error) {
-        // TODO: manage network unavailable or CORS “Access-Control-Allow-Origin”
-        // https://github.com/ollama/ollama/blob/main/docs/faq.md#how-can-i-allow-additional-web-origins-to-access-ollama
-        // e.g. launchctl setenv OLLAMA_ORIGINS "moz-extension://*"
-        // https://github.com/ollama/ollama/blob/main/docs/faq.md#how-do-i-configure-ollama-server
+                selectOllamaModel.appendChild(option)
+            })
+        }
+        else {
+            // A specific message is displayed to indicate that no model was
+            // found in Ollama.
+            document.querySelector('#ollama .description.ollama-warning-no-model').classList.add('show')
+        }
+    }
+    // Error handling: possible cases could be related to the fact that Ollama
+    // is not running, making the APIs unavailable, or that CORS has not been
+    // enabled on these APIs, resulting in an "Access-Control-Allow-Origin"
+    // error, see:
+    //
+    //   1. https://github.com/ollama/ollama/blob/main/docs/faq.md#how-can-i-allow-additional-web-origins-to-access-ollama
+    //   2. https://github.com/ollama/ollama/blob/main/docs/faq.md#how-do-i-configure-ollama-server
+    //
+    // So for exmaple in a macOS environment, the following command should be
+    // executed: launchctl setenv OLLAMA_ORIGINS "moz-extension://*"
+    catch (error) {
+        document.querySelector('#ollama .description.ollama-error-api').classList.add('show')
     }
 })
